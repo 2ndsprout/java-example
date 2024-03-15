fd29c214c16527157de36e1d3fb3e785296ecf2b
2ndsprout <ralphlauren5243@gmail.com>
2024-03-15 15:44:08 +0900

    exam2 commit

--- src/Exam2.java
+++ src/Exam2.java
@@ -1,2 +1,70 @@
 public class Exam2 {
+    public static void main(String[] args) {
+
+        int 높이 = 3;
+
+        for (int i = 1; i <= 높이; i++) {
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+           for (int e = 2; e <= i; e++) {
+               System.out.print("*");
+           }
+            System.out.println();
+        }
+
+
+        // 높이 : 3
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         */
+
+
+        // 높이 : 5
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         */
+
+        // 높이 : 7
+
+        // 출력
+
+        /*
+
+         *
+
+         ***
+
+         *****
+
+         *******
+
+         *********
+
+         ***********
+
+         *************
+
+         */
+    }
 }
